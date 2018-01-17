73aff6c12520a4b2a98a2b7744e57e66ad76d3f6
sharmilasonavane24 <sharmila.sonavane24@gmail.com>
2018-01-17 11:30:09 +0100

    Added controllers and views

---
+++ HospitalManagment/Controllers/LabourDetailController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManagment.Controllers
+{
+    public class LabourDetailController : Controller
+    {
+        // GET: LabourDetail
+        public ActionResult LabourDetail()
+        {
+            return View();
+        }
+
+        // GET: LabourDetail/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LabourDetail/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LabourDetail/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LabourDetail/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LabourDetail/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LabourDetail/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LabourDetail/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ HospitalManagment/Controllers/MonitoringChartController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManagment.Controllers
+{
+    public class MonitoringChartController : Controller
+    {
+        // GET: MonitoringChart
+        public ActionResult MonitoringChart()
+        {
+            return View();
+        }
+
+        // GET: MonitoringChart/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MonitoringChart/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MonitoringChart/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MonitoringChart/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MonitoringChart/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MonitoringChart/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MonitoringChart/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
