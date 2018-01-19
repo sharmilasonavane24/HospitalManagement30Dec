4549987debcb278ed77249e2b7643aac13ee840f
sunitakawale <34807448+sunitakawale@users.noreply.github.com>
2018-01-19 10:28:25 +0530

    Resolved unit testing defects.. Please retest and let me know asap.

--- HospitalManagment/CustomValidation/DateValidations.js
+++ HospitalManagment/CustomValidation/DateValidations.js
@@ -7,6 +7,24 @@ $(document).ready(function () {
         changeMonth: true,
         changeYear: true,
         maxDate: -0,
-        yearRange: "1900:" + currentYear
+        yearRange: "1900:" + currentYear,
+        onSelect: function (dateText) {
+            if (dateText != null) {
+                 
+                $("#Age").val(getAge(dateText));
+            }
+        }
     });
 });
+
+function getAge(dateString) {
+    debugger;
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m == 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
